1190a8d29cd66bc389735e91b16863d0a4ebf4a2
speedingman <hcdo@andats.com>
2018-12-16 14:19:19 +0900

    new activity

---
+++ app/src/main/java/com/example/zeros/login/SignUpActivity.java
@@ -0,0 +1,31 @@
+package com.example.zeros.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        bindViews();
+        setupEvents();
+        setValuse();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValuse() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
