ad092348fa44d3de1ceb69047dd7090ca610717f
speedingman <hcdo@andats.com>
2018-12-16 15:50:32 +0900

    회원가입기능 마무리
    --  아이디 @ 포함하는지 검토
    --  비밀번호가 8자 이상인지
    -- 새액티비티를 만들때 기존 화면 날리기까지

--- app/src/main/java/com/example/zeros/login/SignUpActivity2.java
+++ app/src/main/java/com/example/zeros/login/SignUpActivity2.java
@@ -1,9 +1,18 @@
 package com.example.zeros.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity2 extends BaseActivity {
+
+    EditText emailTxt;
+    EditText pasTxt;
+    Button comBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,32 @@ public class SignUpActivity2 extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        comBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!emailTxt.getText().toString().contains("@")){
+                    Toast.makeText(mContext, "이메일 형식으로 입력해주세요", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(pasTxt.getText().toString().length() < 8){
+                    Toast.makeText(mContext, "8글자 이상ㅇ으로 입력해주세요", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Toast.makeText(mContext, "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show();
+
+
+
+                // 기존 모든 화면을 지우기
+
+
+                Intent intent = new Intent(mContext, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP| Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        });
 
     }
 
@@ -26,6 +61,11 @@ public class SignUpActivity2 extends BaseActivity {
 
     @Override
     public void bindViews() {
+
+        emailTxt = findViewById(R.id.emailTxt);
+        pasTxt = findViewById(R.id.pasTxt);
+        comBtn = findViewById(R.id.comBtn);
+
 
     }
 }
